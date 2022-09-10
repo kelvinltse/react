8601bcd218c6f5f9b4967891a4e2aa10dcd9ebe1
Kelvin Tse <kelvin@kelvinsmbp2021.lan>
2022-09-10 16:15:17 -0700

    added more colors and used const

--- temperature-control-local/src/App.jsx
+++ temperature-control-local/src/App.jsx
@@ -9,36 +9,52 @@ export default function App() {
   )
 }
 
+const tempColors = { //background colors as const
+  hundred: '#de0000',
+  ninety: '#e7001f',
+  eighty: '#ee0036',
+  seventy: '#f0004e',
+  sixty: '#ed0068',
+  fifty: '#e40085',
+  forty: '#d200a4',
+  thirty: '#b500c4',
+  twenty: '#8700e2',
+  ten: '#0000ff',
+  zero: '#000000',
+};
+
 class Thermostat extends Component {
   constructor(props) {
     super(props);
     this.state = {
       temperature: 65,
       thermStyle: {
-        backgroundColor: 'green'
+        backgroundColor: tempColors.sixty
       }
     };
   }
 
-  setColor = () => { 
-    if (this.state.temperature < 60) {
-      this.setState({ thermStyle: {backgroundColor: 'blue'}});
-    } else if (this.state.temperature <70) {
-      this.setState({ thermStyle: {backgroundColor: 'green'}});
-    } else {
-      this.setState({thermStyle: {backgroundColor: 'red'}});
-    } 
-    console.log(this.state.temperature);
+  setColor = () => {  //background colors as const
+    let temp = this.state.temperature;
+    if (temp < 10) {this.setState({ thermStyle: {backgroundColor: tempColors.zero}});} else 
+    if (temp < 20) {this.setState({ thermStyle: {backgroundColor: tempColors.ten}});} else 
+    if (temp < 30) {this.setState({ thermStyle: {backgroundColor: tempColors.twenty}});} else 
+    if (temp < 40) {this.setState({ thermStyle: {backgroundColor: tempColors.thirty}});} else 
+    if (temp < 50) {this.setState({ thermStyle: {backgroundColor: tempColors.forty}});} else 
+    if (temp < 60) {this.setState({ thermStyle: {backgroundColor: tempColors.fifty}});} else 
+    if (temp < 70) {this.setState({ thermStyle: {backgroundColor: tempColors.sixty}});} else 
+    if (temp < 80) {this.setState({ thermStyle: {backgroundColor: tempColors.seventy}});} else 
+    if (temp < 90) {this.setState({ thermStyle: {backgroundColor: tempColors.eighty}});} else 
+    if (temp < 100) {this.setState({ thermStyle: {backgroundColor: tempColors.ninety}});} else 
+    {this.setState({ thermStyle: {backgroundColor: tempColors.hundred}});}
   }
 
   decrease = () => {
     this.setState({ temperature: this.state.temperature - 1 }, this.setColor); //solves for async setState by passing setColor as callback
-    console.log(this.state.temperature); //temperature state shows original temp rather than new temperature
   }
 
   increase = () => {
     this.setState({ temperature: this.state.temperature + 1 }, this.setColor);
-    console.log(this.state.temperature);
   }
 
   render() {
