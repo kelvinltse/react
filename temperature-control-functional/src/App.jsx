0949fce3dba3eab9fe826f23bfbed65957c1676b
Kelvin Tse <kelvin@kelvinsmbp2021.lan>
2022-09-19 21:33:43 -0700

    functional now rendering, cannot set initial value of temp color

---
+++ temperature-control-functional/src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import './App.css'
+
+export default function App() {
+//function App() {
+
+//set state with const
+  const [temperature, setTemperature] = useState(65);
+  const [thermStyle, setThermStyle] = useState({backgroundColor: '#000000'}); //issue is here - cannot set default state with variable?
+  //const [thermStyle, setThermStyle] = useState(() => ({backgroundColor: tempColors.sixty}));
+
+  const tempColors = { //background colors as const
+    hundred: '#de0000',
+    ninety: '#e7001f',
+    eighty: '#ee0036',
+    seventy: '#f0004e',
+    sixty: '#ed0068',
+    fifty: '#e40085',
+    forty: '#d200a4',
+    thirty: '#b500c4',
+    twenty: '#8700e2',
+    ten: '#0000ff',
+    zero: '#000000',
+  };
+
+  function setColor() {  //background colors as const
+    let temp = temperature;
+
+    //this.setState -> setThermStyle({backgroundColor: tempColors.zero})
+    if (temp < 10) {setThermStyle({backgroundColor: tempColors.zero});} else 
+    if (temp < 20) {setThermStyle({backgroundColor: tempColors.ten});} else 
+    if (temp < 30) {setThermStyle({backgroundColor: tempColors.twenty});} else 
+    if (temp < 40) {setThermStyle({backgroundColor: tempColors.thirty});} else 
+    if (temp < 50) {setThermStyle({backgroundColor: tempColors.forty});} else 
+    if (temp < 60) {setThermStyle({backgroundColor: tempColors.fifty});} else 
+    if (temp < 70) {setThermStyle({backgroundColor: tempColors.sixty});} else 
+    if (temp < 80) {setThermStyle({backgroundColor: tempColors.seventy});} else 
+    if (temp < 90) {setThermStyle({backgroundColor: tempColors.eighty});} else 
+    if (temp < 100) {setThermStyle({backgroundColor: tempColors.ninety});} else 
+    {setThermStyle({backgroundColor: tempColors.hundred});} 
+  }
+
+  function decrease() {
+    setTemperature(temperature - 1);
+    setColor();
+  }
+
+  function increase() {
+    setTemperature(temperature + 1);
+    setColor();
+    //this.setState({ temperature: this.state.temperature + 1 }, this.setColor); //solves for async setState by passing setColor as callback
+  }
+
+  return ( //return the render here
+    <div className='thermostatUI' style={thermStyle}> 
+      <div className='temp'>
+        {temperature} &#176;F
+      </div>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+    //<button>test??</button>
+  );
+
+// class Thermostat extends Component { //delete class entirely
+//   constructor(props) { 
+//     super(props);
+//     this.state = {
+//       temperature: 65,
+//       thermStyle: {
+//         backgroundColor: tempColors.sixty
+//       }
+//     };
+//   }
+
+
+}
