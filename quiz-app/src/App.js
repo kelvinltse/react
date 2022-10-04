9f051ea095212050a113aee1ab417e31f32a5a61
Kelvin Tse <kelvinltse@gmail.com>
2022-10-03 22:16:37 -0700

    finished and got some gooood colors

---
+++ quiz-app/src/App.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+
+export default function App() {
+
+  function handleClick(answerOption, event) {
+    let nextQuestionNumber = questionNumber + 1;
+    setQuestionNumber(nextQuestionNumber);
+    if(questions[questionNumber].answerOptions[answerOption].isCorrect) {
+      setScore(score + 1);
+    }
+    if(nextQuestionNumber === questions.length) {
+      setGameIsOver(true);
+    }
+  }
+
+  const [score, setScore] = useState(0);
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [gameIsOver, setGameIsOver] = useState(false);
+
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+
+	return (
+		<div className='app'>
+			{/* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+			{gameIsOver ? (
+				<div className='score-section'>You scored {score} out of {questions.length}</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {questionNumber + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[questionNumber].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						<button onClick={(e) => handleClick(0, e)}>{questions[questionNumber].answerOptions[0].answerText}</button>
+						<button onClick={(e) => handleClick(1, e)}>{questions[questionNumber].answerOptions[1].answerText}</button>
+						<button onClick={(e) => handleClick(2, e)}>{questions[questionNumber].answerOptions[2].answerText}</button>
+						<button onClick={(e) => handleClick(3, e)}>{questions[questionNumber].answerOptions[3].answerText}</button>
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
