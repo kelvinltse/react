70b6784b558882a3762d3a6cf6dbf7e0ac2cdc20
Kelvin Tse <kelvinltse@gmail.com>
2022-10-16 21:53:28 -0700

    added profile json

--- user-login-app/src/App.js
+++ user-login-app/src/App.js
@@ -1,15 +1,19 @@
-
+import { useState } from 'react';
 import './App.css';
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
 export default function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="App">
      ayyy
      <LoginButton />
      <LogoutButton />
+     <Profile />
     </div>
   );
 }
--- user-login-app/src/index.js
+++ user-login-app/src/index.js
@@ -6,7 +6,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
+    <Auth0Provider //github login
       domain="dev-65w7gbnr.us.auth0.com"
       clientId="iXBEmYgMIHEva2AKuj0bRdKtkJM1W5ia"
       redirectUri={window.location.origin}
