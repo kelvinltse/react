3a5c5d55fc92efe4fc2f77bb22803e849032b675
Kelvin Tse <kelvinltse@gmail.com>
2022-10-23 21:53:09 -0700

    added form

---
+++ todo-list/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>To-do List</h1>
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
