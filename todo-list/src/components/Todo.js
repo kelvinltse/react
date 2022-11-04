376201b210aa4f6f4cd4cb8fdf71dee2b46acda7
Kelvin Tse <kelvinltse@gmail.com>
2022-11-03 22:37:25 -0700

    working on delete handling

--- todo-list/src/components/Todo.js
+++ todo-list/src/components/Todo.js
@@ -1,14 +1,22 @@
 import React from "react";
 
-const Todo = ( {text} ) => {
+const Todo = ( {text, todo, todos, setTodos} ) => {
+
+    const handleClick = () => {
+        
+    }
+
+    const handleDelete = () => {
+        setTodos(todos.filter( (el) => el.id !== todo.id));
+    }
 
     return ( 
         <div className="todo"> 
             <li className="todo-item">{text}</li>
-            <button className="complete-btn">
+            <button className="complete-btn" onClick={handleClick}>
                 <i className="fas fa-check"></i>
             </button>
-            <button className="trash-btn">
+            <button className="trash-btn" onClick={handleDelete}>
                 <i className="fas fa-trash"></i>
             </button>
         </div>
